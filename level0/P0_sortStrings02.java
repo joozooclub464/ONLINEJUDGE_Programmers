21ddbbfb71d6eea2bef4eda96e16d2132b45e9e6
joozooclub464 <86102525+joozooclub464@users.noreply.github.com>
2023-04-01 00:33:58 +0900

    문자열 정렬하기 (2)

--- level0/P0_sortStrings02.java
+++ level0/P0_sortStrings02.java
@@ -12,3 +12,6 @@ class Solution {
         return answer;
     }
 }
+//처음에는 아스키 코드 사용해서 대문자 추출 후 소문자로 바꿔주려고 했었음.
+//갑자기 uppercase가 생각나서 방법을 바꿈.
+//split 한 후 소문자로 바꾸고 나서 sort하려 했으나 검색찬스를 써서 str.toLowerCase().split("")으로 바로 배열에 넣고 sort해줌
